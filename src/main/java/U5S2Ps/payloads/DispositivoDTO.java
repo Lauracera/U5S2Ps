ed7eb3c3adaadf42cade2c7096675e311561e438
Laura Ceragioli <141077304+Lauracera@users.noreply.github.com>
2024-02-16 15:01:43 +0100

    commit dto dispositivo

--- src/main/java/U5S2Ps/payloads/DispositivoDTO.java
+++ src/main/java/U5S2Ps/payloads/DispositivoDTO.java
@@ -1,4 +1,14 @@
 package U5S2Ps.payloads;
 
-public class DispositivoDTO {
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
+public record DispositivoDTO(
+        @NotEmpty(message = "Il tipo di dipositivo è obbligatorio!")
+        @Size(min = 3, max = 20, message = "I caratteri devono essere compresi tra 3 e 20")
+        String tipo,
+        @NotEmpty(message = "Lo stato del dispositivo è obbligatorio!")
+        @Size(min = 8, max = 12, message = "I caratteri devono essere compresi tra 8 e 12")
+        String stato
+) {
 }
